86d5c78455c68d039220a06255f0dd9decb57e9c
Taylor Kraft <taylorkraft16@gmail.com>
2020-01-02 15:06:05 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -31,11 +31,11 @@ def can_be_created_in_a_block(title: "Home Alone", release_date: 1990)
   # release_date == 1990
 
   Movie.create do |m|
-    m.string :title
-    m.integer :release_date
-    m.string :director
-    m.string :lead
-    m.boolean :in_theaters
+    m.string = :title
+    m.integer = :release_date
+    m.string = :director
+    m.string = :lead
+    m.boolean = :in_theaters
   end
 end
 
