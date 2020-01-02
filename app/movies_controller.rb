096aa237efc618c90968004aab579e3156303f3b
Taylor Kraft <taylorkraft16@gmail.com>
2020-01-02 14:55:54 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -97,7 +97,7 @@ end
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once")
   movie = Movie.find_by(title: "That One Where the Guy Kicks Another Guy Once")
-  movie.destroy(title: "That One Where the Guy Kicks Another Guy Once")
+  movie.destroy(1)
 end
 
 def can_destroy_all_items_at_once
