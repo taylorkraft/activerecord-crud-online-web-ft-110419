ec194a68ffc7c010388c94e8b35c3adf22bc3c6f
Taylor Kraft <taylorkraft16@gmail.com>
2020-01-02 14:36:27 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -76,7 +76,7 @@ def can_be_found_updated_and_saved
   Movie.create(title: "Awesome Flick")
   movie = Movie.find_by(title: "Awesome Flick")
   movie = Movie.update(title: "Even Awesomer Flick")
-  movie.save
+  movie.save(false)
 end
 
 def can_update_using_update_method
